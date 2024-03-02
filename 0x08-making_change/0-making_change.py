a7f1857f16682ff860009eb51a048e70da7b5654
sapera11 <akinsetewilliams@gmail.com>
2024-03-02 11:43:39 -0800

    updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,11 +6,11 @@ def makeChange(coins, total):
         dp = [total + 1] * (total + 1)
             dp[0] = 0
 
-                for a in range(1, total + 1):
+            for a in range(1, total + 1):
                 for coin in coins:
-                if a - coin >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - coin])
+                    if a - coin >= 0:
+                       dp[a] = min(dp[a], 1 + dp[a - coin])
 
-                if dp[total] != (total + 1):
-                return dp[total]
-                return -1
+                    if dp[total] != (total + 1):
+                       return dp[total]
+                    return -1
